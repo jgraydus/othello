43771a32e7af2c959eee0b4278460e253ebda18f
josh <jgraydus@gmail.com>
2014-06-07 18:52:33 -0400

    updated documentation

--- src/othello/game.clj
+++ src/othello/game.clj
@@ -2,14 +2,19 @@
   [:use [othello board engine]])
 
 (defn tally-results
-  "returns a map of colors to number of positions occupied by the color"
+  "returns a map from color to number of board positions occupied by
+   that color"
   [board]
   (let [occupied-positions (get-occupied-positions board)
         tally (group-by #(get-position board %) occupied-positions)]
     (apply merge (for [k (keys tally)] {k (count (k tally))}))))
 
 (defn play-game
-  ""
+  "main game loop.  players is a map where the keys :white and :black
+   correspond to the functions that should be called to generate that
+   player's next move. pass is a boolean that indicates whether or not
+   the last turn was passed, i.e. the player had no move.  the game ends
+   when neither player can move"
   [board players color pass]
   (let [possible-moves (find-eligible-moves board color)
         should-pass (empty? possible-moves)
