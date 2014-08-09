26f2461e93f757161f204bd636774a5126783325
josh <jgraydus@gmail.com>
2014-08-09 09:25:00 -0400

    bug in min-max-value.  attempt to develop better heuristic

--- src/othello/player.clj
+++ src/othello/player.clj
@@ -2,6 +2,9 @@
   [:use [othello board engine game]
         [clojure.data.finger-tree]])
 
+(declare min-max-value)
+(declare min-max)
+
 (defn dumb-player
   "A simple player implementation for testing."
   [board color]
@@ -12,17 +15,29 @@
   [board color]
   (rand-nth (find-eligible-moves board color)))
 
-
-(defn smart-player [depth score]
-  (fn [board color]
-
-    ))
-
 (defn count-whites [board]
   (count (filter #(= :white %) (vals (:data board)))))
 
+(defn score-1 [board]
+  (let [black-positions (filter #(= :black %) (:data board))
+        white-positions (filter #(= :white %) (:data board))
+        blacks (count black-positions)
+        whites (count white-positions)
+        ratio-score (int (* 100 (/  (Math/abs (- blacks whites)) (+ blacks whites 1))))
+        corners (filter #{[0 0] [0 7] [7 0] [7 7]} white-positions)
+        worst-positions (filter #{[1 0] [0 1] [1 1]
+                                  [6 0] [6 1] [7 1]
+                                  [0 6] [1 6] [1 7]
+                                  [6 6] [7 6] [6 7]} white-positions)]
+    (+ ratio-score (* 200 (count corners)) (* -200 (count worst-positions)))))
+
 (defn min-max [board color score-function depth]
   (:move (min-max-value {:score 0 :board board} color score-function depth)))
+
+
+(defn smart-player [board color]
+  (min-max board color score-1 3))
+
 
 (defn apply-score [original-board new-move color score-function]
   (let [new-board (execute-move original-board new-move color)]
@@ -34,14 +49,16 @@
   (if (= 0 depth) all
       (let [apply-score (fn [original-board new-move]
                           (let [new-board (execute-move original-board new-move color)]
-                              {:score (+ (* (score-function new-board) (color {:white 1 :black -1}))
-                                         (:score (min-max-value {:score 0 :board new-board}
-                                                                (color {:white :black :black :white})
-                                                                score-function
-                                                                (dec depth))))
+                            {:score ((fnil + 0 0)
+                                     (* (score-function new-board) (color {:white 1 :black -1}))
+                                     (:score (min-max-value {:score 0 :board new-board}
+                                                            (color {:white :black :black :white})
+                                                            score-function
+                                                            (dec depth)))
+                                       score 0)
                                :move new-move
                                :board new-board}))
             potential-moves (find-eligible-moves board color)
-            scored-moves (map #(apply-score board %) potential-moves)
+            scored-moves (pmap #(apply-score board %) potential-moves)
             comp (color {:white > :black <})]
         (first (sort-by :score comp scored-moves)))))
