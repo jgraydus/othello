856f7018236f0f2520880d18a007db4d10e2bb2e
josh <jgraydus@gmail.com>
2014-08-16 11:58:37 -0400

    starting overhaul to address performance issues

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject othello "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "othello"
+  :url "https://github.com/jgraydus/othello"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.finger-tree "0.0.2"]]
   :main ^:skip-aot othello.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
